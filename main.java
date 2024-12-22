c66769b40518556c3765718b60f3ca2492dd5997
PriyankaDev077 <ikori41@gmail.com>
2024-12-22 17:25:11 +0530

    string methods in java

---
+++ main.java
@@ -0,0 +1,15 @@
+class main{
+    public static void main(String[] args) {
+        String x="cod";
+        String y="ingal";
+        String z=x+y;
+        System.out.println(z);
+        String a=z.toUpperCase();
+        System.out.println(a);
+        String b="RABBIT";
+        String c=b.toLowerCase();
+        System.out.println(c);
+        System.out.println(a.length());
+        System.out.println(b.length());
+    }
+}
